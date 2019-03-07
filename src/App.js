5fa1b35717bcbbf138a140137cae34cf3f674b70
jstrom2002 <jstrom2002@hotmail.com>
2019-03-06 23:41:58 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
-//Referencing BarChart.js in App.js
-
 import React, { Component } from 'react'
 import './App.css'
-import BarChart from './BarChart'
-import { parseXLSX} from "./xlsParser";
-//import { parseXLSX } from './xlsParser'
-//import XMLloader from './XMLloader'
 
 class App extends Component {
   render() {
@@ -14,17 +8,8 @@ class App extends Component {
           <div className='App-header'>
             <h2>CMPT 495 Capstone Project</h2>
           </div>
-            <script>parseXLSX()</script>
-            Bar Chart 1
-          <div>
-              <BarChart data={[5,10,1,3]} size={[600,200]} /></div>
-            Bar Chart 2
-            <div><BarChart data={[4,3.94,5,5,4,3,1,2,1,4,6,4,,2.3,5,13]} size={[600,200]} /></div>
-            Bar Chart 3
-            <div><BarChart data={[5,3.9,3,2.8,2]} size={[600,200]} /></div>
-          </div>
+        </div>
     )
   }
 }
-
 export default App

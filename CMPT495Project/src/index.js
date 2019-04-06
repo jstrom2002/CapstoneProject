894a490d4e7b1d720b507a9912c00ad9a036ea3a
Jeffrey Strom <jstrom2002@hotmail.com>
2019-04-06 10:12:23 -0600

    file delete
    deleted undesired files

--- CMPT495Project/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import app from "./component/app";
-import "./index.css";
-
-ReactDOM.render(<app />, document.getElementById("root"));
